d4397ca3c2ab3e0187c7546e837729e7225aa8c4
Numian <takatsho@gmail.com>
2017-08-05 14:02:41 +0200

    Pull commit

--- main/Main.py
+++ main/Main.py
@@ -1,4 +1,20 @@
-from main.Speaker import Speaker
+file = open("targetfile.csv", "r")
+results = open("results.txt", "w")
 
-s = Speaker()
-s.printHello()
+file.seek(0, 0)
+lines = file.readlines()
+
+canWrite = False
+numberOfTable = 0
+
+for line in lines:
+    if (line.strip("\n") == "END_TABLE") & (canWrite):
+        break
+    if canWrite:
+        results.write(line)
+    if line.strip("\n") == "START_TABLE":
+        numberOfTable += 1
+        if numberOfTable == 2:
+            canWrite = True
+
+file.close()
--- main/Speaker.py
+++
@@ -1,3 +0,0 @@
-class Speaker():
-    def printHello(self):
-        print("Hello Wolrd")
